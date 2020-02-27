89b612319115e00fc65bef0aaba183ae8b68a35c
enahe <enahe@udel.edu>
2020-02-27 18:03:02 -0500

    Began work on game in earnest

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -2,13 +2,22 @@ import ExampleObject from '../objects/exampleObject';
 
 export default class MainScene extends Phaser.Scene {
   private exampleObject: ExampleObject;
+  background: Phaser.GameObjects.Image;
+  ship1: Phaser.GameObjects.Image;
+  ship2: Phaser.GameObjects.Image;
+  ship3: Phaser.GameObjects.Image;
 
   constructor() {
     super({ key: 'MainScene' });
   }
 
   create() {
-    this.exampleObject = new ExampleObject(this, 0, 0);
+    this.background = this.add.image(0,0, "background");
+    this.background.setOrigin(0,0);
+
+    this.ship1 = this.add.image(1024/2 - 50, 1024/2, "ship");
+    this.ship2 = this.add.image(1024/2, 1024/2, "ship2");
+    this.ship3 = this.add.image(1024/2 + 50, 1024/2, "ship3");
   }
 
   update() {
--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -4,6 +4,10 @@ export default class PreloadScene extends Phaser.Scene {
   }
 
   preload() {
+    this.load.image("background", "src/assets/ville.png");
+    this.load.image("ship", "src/assets/Ship1.png");
+    this.load.image("ship2", "src/assets/Ship2.png");
+    this.load.image("ship3", "src/assets/Ship3.png");
   }
 
   create() {
